a82a73bcac55e6dd0faea3ec1e2776ec07ae65ab
ChoiHeeSoo <ssooya90@naver.com>
2019-04-26 17:47:18 +0900

    커밋 확인용2_희수

---
+++ src/kh/git/model/Model.java
@@ -0,0 +1,4 @@
+package kh.git.model;
+
+public class Model {
+}
---
+++ src/kh/git/run/Run.java
@@ -0,0 +1,13 @@
+package kh.git.run;
+
+import kh.git.view.View;
+
+public class Run {
+
+	public static void main(String[] args) {
+
+		View v		=	new View();
+		v.main();
+
+	}
+}
