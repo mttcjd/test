1db4d12e1d637fa7a1852ce1f2503f2ff8b52149
ChoiHeeSoo <ssooya90@naver.com>
2019-04-27 08:20:11 +0900

    커밋쓰
    Signed-off-by: ChoiHeeSoo <ssooya90@naver.com>

--- src/kh/git/view/View.java
+++ src/kh/git/view/View.java
@@ -2,9 +2,20 @@ package kh.git.view;
 
 public class View {
 
+
+	public void view(){
+
+		System.out.println("뷰에요.");
+
+	}
 	public static void main(String[] args) {
 		System.out.println("테스트");
 	}
 
+
+	public void main() {
+		System.out.println("뷰에요.");
+
+	}
 
 }
